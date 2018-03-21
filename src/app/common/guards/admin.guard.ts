01e2ab522d524b0bdf19454c931280615113de92
Jan Milota <jan.milota@vivnetworks.com>
2018-03-21 10:06:44 +0100

    authGuard redirects

--- src/app/common/guards/admin.guard.ts
+++ src/app/common/guards/admin.guard.ts
@@ -1,4 +1,4 @@
-﻿import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+﻿import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../services/auth.service';
 import { Injectable } from '@angular/core';
@@ -6,8 +6,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AdminGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
-  public canActivate = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> => this.authService.isAdmin$;
+  public canActivate = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> =>
+    this.authService.isAdmin$.map(isAdmin => {
+        if (isAdmin) {
+          return true;
+        }
+        else {
+          this.router.navigateByUrl('/');
+          return false;
+        }
+      }
+    );
 }
