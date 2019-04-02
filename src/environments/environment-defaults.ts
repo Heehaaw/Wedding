09a267ebe3ccffc84fe324d3147ce2e0851321e8
Jan Milota <jan.milota@vivnetworks.com>
2019-04-02 16:17:21 +0200

    Hide IDs

--- src/environments/environment-defaults.ts
+++ src/environments/environment-defaults.ts
@@ -2,11 +2,11 @@
   production: false,
   hmr: false,
   firebase: {
-    apiKey: "AIzaSyAd-9medcI4Jc_QOVNEOFsAtU8VLf1-KHc",
+    apiKey: "",
     authDomain: "wedding-2c80b.firebaseapp.com",
     databaseURL: "https://wedding-2c80b.firebaseio.com",
     projectId: "wedding-2c80b",
     storageBucket: "wedding-2c80b.appspot.com",
-    messagingSenderId: "780304182621"
+    messagingSenderId: ""
   }
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,11 @@ export const environment = {
   production: true,
   hmr: false,
   firebase: {
-    apiKey: 'AIzaSyAd-9medcI4Jc_QOVNEOFsAtU8VLf1-KHc',
+    apiKey: '',
     authDomain: 'wedding-2c80b.firebaseapp.com',
     databaseURL: 'https://wedding-2c80b.firebaseio.com',
     projectId: 'wedding-2c80b',
     storageBucket: 'wedding-2c80b.appspot.com',
-    messagingSenderId: '780304182621'
+    messagingSenderId: ''
   }
 };
